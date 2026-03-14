b863491ffe118029f28daab86f98e15853fa0c21
zakariaFrontDev <zakaria.aitbibote@gmail.com>
2026-03-14 00:59:29 +0000

    test

--- public/api.js
+++ public/api.js
@@ -22,6 +22,7 @@ export async function generate() {
     genirateFunc()
     const data = await res.json()
     if (!data.success || !data.images || !data.images.length) {
+      errorMsgFunc("something went wrong please try again")
       throw new Error("Image generation failed on server");
     }
     genirateHtmlForImgsFunc(data)
